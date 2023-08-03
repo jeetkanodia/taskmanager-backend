33270030f94f159b4040fd7d61d0c52d955a9fa6
Jeet Kanodia <jeetkanodia@gmail.com>
2023-08-04 02:08:07 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes/TaskRoute')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.port || 5000
+
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>{
+    console.log("Connected to MongoDB")
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+app.use(routes)
+
+app.listen(PORT,()=>
+{
+    console.log(`listening on port ${PORT}`)
+})
